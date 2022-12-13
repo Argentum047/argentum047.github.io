074082cfc767ab2cd03f9ac5fa3dafe8226af778
Argentum <68360814+Argentum047@users.noreply.github.com>
2022-12-13 10:28:12 -0500

    Deletion of some old files

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.systemd
-    pkgs.sudo
-    pkgs.python39Packages.pip
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
